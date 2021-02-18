6411aadb17462e8a2d5aa651969a59c04907f16c
pc <kumarmukeshpatel57@gmail.com>
2021-02-18 16:23:53 +0530

    Reverse Number

---
+++ reverse_number/ReverseNumber.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+
+    cout<<"Enter Number: ";
+    cin>>n;
+
+    int reverse;
+    while (n>0)
+    {
+        int lastdigit = n%10;
+        reverse = reverse*10 + lastdigit;
+        n = n/10;
+
+    }
+
+    cout<<"Reverse Number is: "<<reverse<<endl;
+    
+    
+    return 0;
+}
