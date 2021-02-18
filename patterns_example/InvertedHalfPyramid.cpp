1c2eba61ca860ddc140e87508418fe5c40fc3eda
pc <kumarmukeshpatel57@gmail.com>
2021-02-18 12:03:49 +0530

    Print "*" Inverted Half Pyramid

---
+++ patterns_example/InvertedHalfPyramid.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num;
+
+    cout<<"Enter the Number : ";
+    cin>>num;
+
+    for (int i = num; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<"* ";   
+        }
+        cout<<endl;
+    } 
+     
+    return 0;
+}
