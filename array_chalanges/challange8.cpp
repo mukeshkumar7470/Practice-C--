2f78a8727c140a5e854333833b124d6aa48ce7a6
pc <kumarmukeshpatel57@gmail.com>
2021-02-26 21:16:15 +0530

    Adding  Kadane’s Algorithm

--- array_chalanges/challange8.cpp
+++ array_chalanges/challange8.cpp
@@ -107,6 +107,9 @@ int main(){
     
     /* ------------------------------------------------------- */
 
+
+    //Kadane’s Algorithm
+    
     int currSum = 0;
     int maxSum = INT_MIN;
     for (int i = 0; i < n; i++)
