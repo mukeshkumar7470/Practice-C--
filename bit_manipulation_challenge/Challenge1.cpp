51aabcb4b71c8f20fa336aea938b21ada02f035d
pc <kumarmukeshpatel57@gmail.com>
2021-03-05 16:30:13 +0530

    challenges

---
+++ bit_manipulation_challenge/Challenge1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+
+//Write a program to find a unique number in an array where all numbers except one, are present twice.
+
+/* Write a program to find a unique number in an array where all numbers except
+one, are present twice.
+Hint: A ⊕ B ⊕ B ⊕ A ⊕ C = C. All those numbers which occur twice will get nullified
+after ⊕ operation. */
+
+
+int unique(int arr[], int n){
+
+    int xorsum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        xorsum = xorsum ^ arr[i];
+    }
+    return xorsum;
+    
+}
+
+int main(){
+    int arr[] = {1, 2, 3, 4, 1, 2, 3};
+
+    cout<<unique(arr, 7)<< endl;
+
+
+    return 0;
+}
