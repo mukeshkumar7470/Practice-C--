b69cba2681bbc85f3feccb9ec55f8f7eaf50f081
pc <kumarmukeshpatel57@gmail.com>
2021-03-09 15:47:09 +0530

    Recursion

---
+++ Recursion/RecursiveSum.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+/* Recursive sum */
+
+
+int sum(int n){
+    if (n == 0)
+    {
+        return 0;
+    }
+
+    int prevSum = sum(n-1);
+    return n + prevSum; 
+    
+}
+
+
+int main(){
+     int n;
+     cin>>n;
+
+     cout<<sum(n)<<endl;
+
+    return 0;
+}         
